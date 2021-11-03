9a926cbf64606363a683371cb12f3d5ddd137fa9
vilkas99 <victorium99@hotmail.com>
2021-11-03 08:28:40 -0600

    Done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={`${process.env.REACT_APP_CONTENT_HOST}${logo}`}
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Este es un microFront!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
