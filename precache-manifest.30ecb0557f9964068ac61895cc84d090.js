5eff61bea8f0f6506108f5e012484670e92857fe
vilkas99 <victorium99@hotmail.com>
2021-11-08 12:49:46 -0600

    Updates

---
+++ precache-manifest.30ecb0557f9964068ac61895cc84d090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f18703343641a9dda1bd6eebf053ff5",
+    "url": "/MicroFront_Microservices/index.html"
+  },
+  {
+    "revision": "a3631721f73ac7ccb61c",
+    "url": "/MicroFront_Microservices/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "cac71651bd08207cfb0b",
+    "url": "/MicroFront_Microservices/static/js/2.376fe879.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MicroFront_Microservices/static/js/2.376fe879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3631721f73ac7ccb61c",
+    "url": "/MicroFront_Microservices/static/js/main.93f53e47.chunk.js"
+  },
+  {
+    "revision": "56a16391e4a656ca60ae",
+    "url": "/MicroFront_Microservices/static/js/runtime-main.c790f089.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MicroFront_Microservices/static/media/logo.ee7cd8ed.svg"
+  }
+]);
